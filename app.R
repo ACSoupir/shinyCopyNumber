d4900126661a959e1d044a6a52dd3490db02507d
Alex Soupir <alex.soupir@sdstate.edu>
2020-11-27 22:42:25 -0500

    add header

--- app.R
+++ app.R
@@ -10,49 +10,50 @@
 library(shiny)
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Upload Data"),
+ui <- navbarPage("Shiny Copy Numbers!",
 
     # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            fileInput("file1", "Choose CSV File",
-                      multiple = TRUE,
-                      accept = c("text/csv/tsv",
-                                 "text/comma-separated-values/tab-separated",
-                                 c(".csv",".tsv"))),
+    tabPanel("Upload Data",
+             fileInput("file1", "Choose CSV File 1",
+                       multiple = TRUE,
+                       accept = c("text/csv/tsv",
+                                  "text/comma-separated-values/tab-separated",
+                                  c(".csv",".tsv"))),
+             fileInput("file2", "Choose CSV File 2",
+                       multiple = TRUE,
+                       accept = c("text/csv/tsv",
+                                  "text/comma-separated-values/tab-separated",
+                                  c(".csv",".tsv"))),
         
-        tags$hr(),
+             tags$hr(),
+         
+             checkboxInput("header", "Header", TRUE),
         
-        checkboxInput("header", "Header", TRUE),
+             radioButtons("sep", "Separator",
+                          choices = c(Comma = ",",
+                                      Semicolon = ";",
+                                      Tab = "\t"),
+                          selected = ","),
+             tags$hr(),
         
-        radioButtons("sep", "Separator",
-                     choices = c(Comma = ",",
-                                 Semicolon = ";",
-                                 Tab = "\t"),
-                     selected = ","),
-        tags$hr(),
-        
-        radioButtons("quote", "Quote",
+             radioButtons("quote", "Quote",
                      choices = c(None = "",
                                  "Double Quote" = '"',
                                  "Single Quote" = "'"),
                      selected = '"'),
-        tags$hr(),
-        
-        radioButtons("disp", "Display",
-                     choices = c(Head = "head",
-                                 All = "all"),
-                     selected = "head")
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            # Output: Data file ----
-            tableOutput("contents")
-        )
+             tags$hr(),
+         
+             radioButtons("disp", "Display",
+                          choices = c(Head = "head",
+                                      All = "all"),
+                          selected = "head")
+             ),
+    tabPanel("Merge Tables",
+             
+    ),
+    
+    mainPanel(
+        tableOutput("contents")
     )
 )
 
